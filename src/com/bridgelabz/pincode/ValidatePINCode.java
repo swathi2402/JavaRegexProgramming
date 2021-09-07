ed364cd2d641e4eeb71086c9ba61c68ab9744ad0
Swathi <swathi.h@perfios.com>
2021-09-07 20:27:47 +0530

    ADDED: Method to validate pin code using regex

---
+++ src/com/bridgelabz/pincode/ValidatePINCode.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.pincode;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidatePINCode {
+
+	private static boolean validatePINCode(String pinCode) {
+		String regex = "[0-9]{6}";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(pinCode);
+		return matcher.matches();
+		
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Enter the PIN code: ");
+		String pinCode = scanner.next();
+		System.out.println("Is the pin code valid?");
+		if(validatePINCode(pinCode))
+			System.out.println("PIN code is valid");
+		else
+			System.out.println("PIN code is not valid");
+		scanner.close();		
+	}
+}
