e8d6c638b9cee866c8383096fe22dcb52c20e259
Swathi <swathi.h@perfios.com>
2021-09-07 20:59:21 +0530

    MODIFIED: Making pin code valid with a white space in between

--- src/com/bridgelabz/pincode/ValidatePINCode.java
+++ src/com/bridgelabz/pincode/ValidatePINCode.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 public class ValidatePINCode {
 
 	private static boolean validatePINCode(String pinCode) {
-		String regex = "^[0-9]{6}$";
+		String regex = "(^[0-9]{3})([\\s]?)([0-9]{3}$)";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(pinCode);
 		return matcher.matches();
@@ -17,8 +17,7 @@ public class ValidatePINCode {
 	public static void main(String[] args) {
 		Scanner scanner=new Scanner(System.in);
 		System.out.println("Enter the PIN code: ");
-		String pinCode = scanner.next();
-		System.out.println("Is the pin code valid?");
+		String pinCode = scanner.nextLine();
 		if(validatePINCode(pinCode))
 			System.out.println("PIN code is valid");
 		else
