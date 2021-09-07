853dea4a70a21057c0d915a6dcbc7f76bedbbe19
Swathi <swathi.h@perfios.com>
2021-09-07 21:10:05 +0530

    ADDED: Validating mandetory first part of the email

---
+++ src/com/bridgelabz/email/ValidateEmail.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.email;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidateEmail {
+	private static boolean validateEmail(String email) {
+		String regex = "([a-zA-Z][a-zA-Z0-9_-]*[a-z|A-Z|0-9])";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(email);
+		return matcher.matches();
+		
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Enter the Email: ");
+		String email = scanner.nextLine();
+		if(validateEmail(email))
+			System.out.println("Valid name");
+		else
+			System.out.println("Invalid name");
+		scanner.close();		
+	}
+
+}
